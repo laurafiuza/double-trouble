59db24922f3464d7e10fb4ad2b619ced2d4c83ee
Gabriel Guimaraes <gabriel@brex.com>
2021-08-29 15:21:50 -0700

    DoubleTroubleOrchestrator compiles

--- contracts/DoubleTroubleOrchestrator.sol
+++ contracts/DoubleTroubleOrchestrator.sol
@@ -1,26 +1,26 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "./DoubleTrouble.sol";
 
 // SPDX-License-Identifier: MIT
 contract DoubleTroubleOrchestrator {
-  /*
   mapping (address => DoubleTrouble) public _troublesomeCollections;
 
-  function makeTroublesomeNft(address nftCollection, uint256 tokenId) {
-    require(IERC721Metadata(nftCollection).supportsInterface(0x80ac58cd),  "collection must refer to an ERC721 address");
-    if (_troublesomeCollections[nftCollection] == address(0)) {
-      makeTroublesomeCollection(nftCollection);
-    }
-
-  }
-
-  function makeTroublesomeCollection(address nftCollection, string memory name, string memory symbol) {
-    require(IERC721Metadata(nftCollection).supportsInterface(0x80ac58cd),  "collection must refer to an ERC721 address");
-    require(_troublesomeCollections[collection] == address(0), "Collection is already Troublesome");
+  function makeTroublesomeCollection(address nftCollection, string memory name, string memory symbol) external {
+    _ensureSupportedNftContract(nftCollection);
+    require(address(_troublesomeCollections[nftCollection]) == address(0), "Collection is already Troublesome");
 
     // Deploy troublesome contract for nftCollection
     _troublesomeCollections[nftCollection] = new DoubleTrouble(name, symbol, nftCollection);
   }
-  */
+
+  function troublesomeCollection(address nftCollection) external view returns (DoubleTrouble) {
+    _ensureSupportedNftContract(nftCollection);
+    return _troublesomeCollections[nftCollection];
+  }
+
+  function _ensureSupportedNftContract(address nftCollection) internal view {
+    require(IERC721Metadata(nftCollection).supportsInterface(0x80ac58cd),  "collection must refer to an ERC721 address");
+  }
 }
---
+++ test/orchestrator.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const DoubleTroubleOrchestrator = artifacts.require("./DoubleTroubleOrchestrator.sol");
+const CryptoPunks = artifacts.require("./CryptoPunks.sol");
+
+const ZERO_ADDR = '0x0000000000000000000000000000000000000000';
+const NON_PRESENT_ID = 79;
+
+contract("DoubleTroubleOrchestrator", accounts => {
+  // TODO: make tokenId the return value of the createNft function
+  // currently, the return value is a transaction object for some reason,
+  // how do we retrieve the return value?
+  var dto, cp;
+
+  before(async () => {
+    cp = await CryptoPunks.deployed();
+    assert.notEqual(cp, undefined, "CryptoPunks contract instance is undefined.");
+    dto = await DoubleTroubleOrchestrator.deployed();
+    assert.notEqual(dt, undefined, "DoubleTroubleOrchestrator contract instance is undefined.");
+    tokenId = 0;
+
+    web3.eth.defaultAccount = accounts[0];
+  });
+
+  it("should work", async () => {
+    assert.equal(await dto.troublesomeCollection(cp.address), ZERO_ADDR, "Initially cp must not have a troublesome collection");
+  });
+});
