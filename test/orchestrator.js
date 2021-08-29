ca0e2ba0f498c6cd403a1dcc4addad063ad9dd9b
Gabriel Guimaraes <gabriel@brex.com>
2021-08-29 16:02:50 -0700

    Done with orchestrator tests

--- test/orchestrator.js
+++ test/orchestrator.js
@@ -1,9 +1,12 @@
 const assert = require('assert');
 const DoubleTroubleOrchestrator = artifacts.require("./DoubleTroubleOrchestrator.sol");
+const DoubleTrouble = artifacts.require("./DoubleTrouble.sol");
 const CryptoPunks = artifacts.require("./CryptoPunks.sol");
 
 const ZERO_ADDR = '0x0000000000000000000000000000000000000000';
-const NON_PRESENT_ID = 79;
+const NAME = "DTCryptoPunks";
+const SYMBOL = "DUNK";
+const NON_NFT_CONTRACT_ADDRESS = "0x52b684d7bDEeB1248b3cc4A4C130A3bED9639f46";
 
 contract("DoubleTroubleOrchestrator", accounts => {
   // TODO: make tokenId the return value of the createNft function
@@ -21,7 +24,26 @@ contract("DoubleTroubleOrchestrator", accounts => {
     web3.eth.defaultAccount = accounts[0];
   });
 
-  it("should work", async () => {
+  it("makeTroublesomeCollection should not work for non ERC721 address", async () => {
+    await assert.rejects(dto.makeTroublesomeCollection(NON_NFT_CONTRACT_ADDRESS, NAME, SYMBOL), /revert/);
+  });
+
+  it("makeTroublesomeCollection should work for NFT contracts", async () => {
     assert.equal(await dto.troublesomeCollection(cp.address), ZERO_ADDR, "Initially cp must not have a troublesome collection");
+
+    const ret = await dto.makeTroublesomeCollection(cp.address, NAME, SYMBOL);
+    assert(ret.receipt.status, true, "Transaction processing failed");
+
+    const dt_address = await dto.troublesomeCollection(cp.address);
+    assert.notEqual(dt_address, undefined, "dt_address is undefined.");
+
+    dt = await DoubleTrouble.at(dt_address);
+    assert.equal(dt_address, dt.address, "Address returned by orchestrator must match dt.deployed().");
+  });
+
+  it("makeTroublesomeCollection should fail if called on same NFT collection twice", async () => {
+    assert.equal(await dto.troublesomeCollection(cp.address), dt.address, "cp must already have a troublesome collection");
+
+    await assert.rejects(dto.makeTroublesomeCollection(cp.address, NAME, SYMBOL), /already Troublesome/);
   });
 });
