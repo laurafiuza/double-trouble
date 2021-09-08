3d14d00a9165edd3bd4202980c2259535aa3a5a6
Laura Fiuza <lauravfiuza@gmail.com>
2021-09-07 20:59:10 -0700

    remove errorcard button

--- client/src/ErrorCard.js
+++ client/src/ErrorCard.js
@@ -2,17 +2,12 @@ import React, { Component } from "react";
 import { Card, Button } from "react-bootstrap";
 
 class ErrorCard extends Component {
-  refreshPage = () => {
-    window.location.reload();
-  };
-
   render() {
     return (
       <Card bg="danger" text="white" style={{width: "18rem"}}>
         <Card.Body>
           <Card.Title>Error</Card.Title>
           <Card.Text>{this.props.error}</Card.Text>
-          <Button variant="light" onClick={() => this.refreshPage}>Go back</Button>
         </Card.Body>
         </Card>
     );
