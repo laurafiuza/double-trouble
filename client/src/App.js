73b9bac73805f6071373f14bc6721f53e3e5a0ac
Gabriel Guimaraes <gabriel@brex.com>
2021-09-12 18:35:47 -0300

    Stylish app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Web3ReactProvider } from '@web3-react/core'
 import { Web3Provider } from '@ethersproject/providers'
+import { Contract } from '@ethersproject/contracts'
 import { useWeb3React } from '@web3-react/core'
 import { InjectedConnector } from '@web3-react/injected-connector'
 import MetamaskProvider from './MetamaskProvider';
+import DoubleTroubleContract from './contracts/DoubleTrouble.json';
+import useSWR from 'swr';
+import { isAddress } from '@ethersproject/address';
+import About from "./About";
+import Find from "./Find";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Page } from './components/base/base'
+import { TopBar } from './components/TopBar'
+import { GlobalStyle } from './global/GlobalStyle'
 
 export const injectedConnector = new InjectedConnector({
   supportedChainIds: [
@@ -22,25 +32,56 @@ function getLibrary(provider) {
   return library
 }
 
+const fetcher = (library, abi, method) => (...args) => {
+  const [arg1, arg2, ...params] = args
+  // it's a contract
+  if (isAddress(arg1)) {
+    const address = arg1
+    const method = arg2
+    const contract = new Contract(address, abi, library.getSigner())
+    return contract[method](...params)
+  }
+  // it's a eth call
+  const method = arg1
+  return library[method](arg2, ...params)
+}
+
+// Switch for chainId
+const dtAddr = '0x610178dA211FEF7D417bC0e6FeD39F05609AD788';
+
 export const Wallet = () => {
+  const { chainId, library, account, activate, active } = useWeb3React()
+  const { data: nfts, mutate } = useSWR([dtAddr, 'allKnownTokens'], fetcher(library, DoubleTroubleContract.abi));
+  console.log(nfts);
+
+  useEffect(() => {
+    // listen for changes on an Ethereum address
+    console.log(`listening for Transfer...`)
+    const contract = new Contract(dtAddr, DoubleTroubleContract.abi, library.getSigner())
+    console.log('effect');
+    console.log(contract);
+    return () => {
+      console.log('unmount');
+    }
+  }, []);
+
+
+  return <div>Wallet on</div>;
+}
+
+export const Connector = () => {
   const { chainId, account, activate, active } = useWeb3React()
 
-  const onClick = () => {
-    activate(injectedConnector)
-  }
-
   return (
-    <div>
-      <div>ChainId: {chainId}</div>
-      <div>Account: {account}</div>
-      {active ? (
-        <div>✅ </div>
-      ) : (
-        <button type="button" onClick={onClick}>
-          Connect
-        </button>
-      )}
-    </div>
+    <Page>
+      <GlobalStyle />
+      <BrowserRouter>
+        <TopBar />
+        <Switch>
+          <Route exact path="/" component={Wallet} />
+        </Switch>
+      </BrowserRouter>
+    </Page>
   )
 }
 
@@ -48,7 +89,7 @@ export const App = () => {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
       <MetamaskProvider>
-        <Wallet />
+        <Connector />
       </MetamaskProvider>
     </Web3ReactProvider>
   )
